a75e1a24c64593c48c6c4753fc914ceffc445aaf
lustforbbiden <chaunceyyilia@gmail.com>
2021-07-11 21:50:04 +0800

    Site updated: 2021-07-11 21:50:04

--- js/matery.js
+++ js/matery.js
@@ -118,8 +118,19 @@ $(function () {
 
     $('.modal').modal();
  // 深色模式设置 
- function switchNightMode() { var body = document.body; if(body.classList.contains('dark')){ document.body.classList.remove('dark'); localStorage.setItem('dark','0'); $('#nightMode').removeClass("fa-lightbulb-o").addClass("fa-moon-o"); return; } else { document.body.classList.add('dark'); localStorage.setItem('dark','1'); $('#nightMode').removeClass("fa-moon-o").addClass("fa-lightbulb-o"); return; } }
-
+ function switchNightMode() { 
+    var body = document.body; 
+    if(body.classList.contains('dark')){ 
+    document.body.classList.remove('dark'); 
+    localStorage.setItem('dark','0'); 
+    $('#nightMode').removeClass("fa-lightbulb-o").addClass("fa-moon-o"); 
+    return; 
+    } else { document.body.classList.add('dark'); 
+    localStorage.setItem('dark','1'); 
+    $('#nightMode').removeClass("fa-moon-o").addClass("fa-lightbulb-o"); 
+    return; 
+    } 
+}
 
     /*回到顶部*/
     $('#backTop').click(function () {
